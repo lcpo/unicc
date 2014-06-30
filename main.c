294533ccefe220c55216d592d7c0c604e8138906
S.S.Korotaev <lcpo.net@yandex.com>
2014-06-30 15:44:31 +0400

    2014-06-30_15:44:31

--- main.c
+++ main.c
@@ -5,8 +5,13 @@
 
 int main(int argc, char **argv) {
 
-
-   printf("%s\n",libc_md5("abc"));
+//REG_NEWLINE|REG_NOTBOL|REG_NOTEOL
+//REG_NOSUB
+//printf("%s\n",libc_md5("abc"));
+char*** out=libc_malloc(sizeof(char***));
+libc_preg_match_all("|<[^>]+>(.*)</[^>]+>|U","<b>пример: </b>\n<div align=left>это тест</div>", out,0);
+printf("%s, %s\n",out[0][0],out[0][1]);
+printf("%s, %s\n",out[1][0],out[1][1]);
 
 
 exit(0);
