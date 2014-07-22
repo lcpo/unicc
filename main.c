26f8a0eaeb293f299477733135f0e2689b5798a9
S.S.Korotaev <lcpo.net@yandex.com>
2014-07-22 12:29:05 +0400

    2014-07-22_12:29:05

--- main.c
+++ main.c
@@ -4,17 +4,38 @@
 //http://netlib.narod.ru/library/book0003/ch08_07.htm
 
 
-//1 =1024 =>4096 	: 1024
-//2 =4097 =>8192 	: 2048
-//3 =8193 =>12288	: 3072
-//4 =12289 =>16384  : 4095
+typedef enum pattern_type{
+START_STR,	
+CLASS_STR,
+MASC_STR,
+STEP_STR,
+END_STR
+				}pattern_type;
+
+typedef struct pattern{
+	pattern_type type;
+	char** section;	
+	int length;
+	}pattern;
 
 
-
-
+void test(char* template,char* str){
+pattern* pt=malloc(sizeof(pattern));
+char* p=template;
+int i=0,n=libc_strlen(template),flag=0;	
+while(i<n){
+//if(p[i]='['){}	
+	i++;}
+	
+	
+return;	
+	}
 
 ///------------------------------------------------------------
 int main(int argc, char** argv) {
+
+test("[0-9]","");
+
 
 /*
 echo ("create array:\n");	
@@ -29,13 +50,13 @@ while(nom<5){test[i][nom]=malloc(2);nom++;}
 free(test);
 echo ("test\n");
 */
-
+/*
 echo ("create array:\n");
 int i=0,nom=0,id=0;
 char* test="123|456|789|abc|def";
 char* test2="afdf|dfdf|avaa|yyy";
 char** ex=libc_explode("|",test);
-char** ex2=libc_explode("|",test2);
+char** ex2=libc_explode("|",test2);*/
 /*
 while(ex[i]!=NULL){
 printf("%s\n",ex[i]);
@@ -46,6 +67,7 @@ while(ex2[i]!=NULL){
 printf("%s\n",ex2[i]);
 i++;}
 */
+/*
 char** ex3=libc_malloc(libc_count((void**)ex)+libc_count((void**)ex2));
 array_merge(ex3,ex,ex2);
 
@@ -65,7 +87,7 @@ printf("%i|%i|%s\n",i,hope.a[i],hope.a[i]);
  
 
 
-
+*/
 
 /*
 id=getptrid(buff);
@@ -95,70 +117,6 @@ return 0;
 
 
 
-/*
-static char mem_block_0[0];
-static char mem_block_1[2];
-static char mem_block_2[4];
-static char mem_block_3[8];
-static char mem_block_4[16];
-static char mem_block_5[32];
-static char mem_block_6[64];
-static char mem_block_7[128];
-static char mem_block_8[256];
-static char mem_block_9[512];
-static char mem_block_10[1024];
-static char mem_block_11[2048];
-static char mem_block_12[4096];
-static char mem_block_13[8190];
-static char mem_block_14[16380];
-static char mem_block_15[32760];
-static char mem_block_16[65520];
-static char mem_block_17[131040];
-static char mem_block_18[262080];
-static char mem_block_19[524160];
-static char mem_block_20[1048320];
-static char mem_block_21[2096640];
-static char mem_block_22[4193280];
-static char mem_block_23[8386560];
-static char mem_block_24[16773120];
-static char mem_block_25[33546240];
-static char mem_block_26[67092480];
-static char mem_block_27[134184960];
-static char mem_block_28[268369920];
-static char mem_block_29[536739840];
-static char mem_block_30[1073479680];
-
-void * salloc(size_t __size){
-void* out=(void*)mem_block_10;
-if(out==NULL){printf("error!\n");return NULL;}
-	return out;
-	}
-
-
-
-
-///------------------------------------------------------------
-
-int main(int argc, char** argv) {
-int i=0;
-char** buff=salloc(10);
-printf("%i\n",libc_count((void**)buff));
-while(i<10){
-buff[i]=libc_itos(i);
-	//printf("%s\n",buff[i]);
-	
-	i++;}	
-i=0;
-while(i<10){
-	printf("%s\n",buff[i]);
-	
-	i++;}	
-
-
-return 0;
-						}
-
-*/
 
 
 
